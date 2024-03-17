d8ddfa8b1a92a7ab63e952a7ab37b991cf454f3b
tsabin2023 <148308137+tsabin2023@users.noreply.github.com>
2024-03-16 18:08:10 -0700

    Update main.py
    updated code and improved ip address.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Tyler Sabin
 # CNE 335 Winter Quarter 2024
-# 3/15/2024
+# 3/16/2024
 # source forked from https://github.com/krhodesrtc/CNE335_automation_project.git
 # source used from Tyler Sabin's fork https://github.com/tsabin2023/CNE335_automation_project.git
 # instructions to complete code https://rtc.instructure.com/courses/2439011/assignments/31799071?module_item_id=79647034
@@ -22,8 +22,12 @@ def print_program_info():
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
-    my_aws_ec2_server = Server("ec2-18-246-238-43.us-west-2.compute.amazonaws.com", r"C:\Users\sabin\OneDrive\Desktop\ts_student_cne340_test.pem")
+    # r for raw file needed, or it won't interpret pem file correctly
+    my_aws_ec2_server = Server("34.218.241.208", r"C:\Users\sabin\OneDrive\Desktop\ts_student_cne340_test.pem")
     # TODO - Call Ping method and print the results
     if my_aws_ec2_server.ping():
         print("code successful")
+        # runs a command on AWS EC2 instance
         my_aws_ec2_server.run_a_command("echo something")
+        # sudo apt -y update && sudo apt -y upgrade
+        print("command successful")
